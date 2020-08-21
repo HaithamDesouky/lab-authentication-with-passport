aea72dc666975ee0421a61ad8bd8a8468192e520
onedweezy <64087376+onedweezy@users.noreply.github.com>
2020-08-21 17:03:11 +0100

    done

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,25 @@
 
 const { Router } = require('express');
 const router = Router();
+const Routeguard = require('../middleware/route-guard');
+
+const roleRouteGuard = roles => {
+  return (req, res, next) => {
+    const role = req.user.role;
+    if (roles.includes(role)) {
+      next();
+    } else {
+      next(new Error('User is not authorized'));
+    }
+  };
+};
 
 router.get('/', (req, res, next) => {
   res.render('index', { title: 'Hello World!' });
+});
+
+router.get('/private', Routeguard, (req, res, next) => {
+  res.render('authentication/private');
 });
 
 module.exports = router;
